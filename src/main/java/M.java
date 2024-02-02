d20f42018575efa52a945b212c98b37dc052c146
Jack Shieh <71810019+sme322-ui@users.noreply.github.com>
2024-02-02 14:56:26 +0800

    Add files via upload

---
+++ src/main/java/M.java
@@ -0,0 +1,12 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
+@EnableAdminServer
+@SpringBootApplication
+public class M{
+	public static void main(String[] args) {
+		SpringApplication.run(M.class,args);
+	}
+}
